095cae0442ef3019f9ad2865666a707d340d216a
Jacob Jørgensen <s175132@student.dtu.dk>
2019-11-28 23:17:58 +0100

    første

---
+++ TEST/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  TEST
+//
+//  Created by Marie Falk on 28/11/2019.
+//  Copyright © 2019 Marie Falk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("lort")
+    }
+
+
+}
+
